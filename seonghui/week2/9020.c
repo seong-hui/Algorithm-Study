00dea8c2b9af5482b6a3ce2195c49e098764594d
seonghui <msh9980@naver.com>
2022-10-11 20:36:55 +0900

    [BOJ] / 골드바흐의 추측 / 35분,
    https://www.acmicpc.net/problem/9020

---
+++ seonghui/week2/9020.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void prime_num(int a[], int n)
+{
+    for(int i =2;i*i<n;i++)
+    {
+        if(a[i] ==0){
+            for(int j = 2; i*j <=n;j++)
+                a[i*j] = 1;
+        }
+    }
+}
+
+int main()
+{
+    int n,m;
+    int a[10000] = {0};
+    a[1] = 1; 
+
+    scanf("%d", &n);
+    for(int i =0 ;i<n;i++)
+    {
+        scanf("%d", &m);
+        prime_num(a, m);
+        for(int j = m/2; j>0; j--)
+        {
+            printf("[%d %d %d]\n", j, m-j, a[j]+a[m-j]);
+            if(a[j]==0 && a[m-j] == 0)
+            {
+                printf("%d %d\n", j, m-j);
+                break;
+            }
+        }
+    }
+
+}
