6d468fe1c5956a0ac0fa648fb440e398303f2fa5
seonghui <msh9980@naver.com>
2022-10-12 12:12:44 +0900

    [BOJ] / 골드바흐의 추측

---
+++ seonghui/week2/6588.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int a[1000001] = {0};
+
+void prime_num(int n)
+{
+    for (int i = 2; i * i <= 100000; i++)
+    {
+        if (a[i] == 0)
+        {
+            for (int j = 2; j * i <= 100000; j++)
+                a[j * i] = 1;
+        }
+    }
+}
+
+int main()
+{
+    int n = 1, cnt = 0;
+    a[1] = 1;
+    prime_num(n);
+    while (n != 0)
+    {
+        scanf("%d", &n);
+        cnt = 0;
+        for (int i = 2; i <= n; i++)
+        {
+            if (a[i] == 0 && a[n - i] == 0)
+            {
+                printf("%d = %d + %d\n", n, i, n - i);
+                cnt++;
+                break;
+            }
+        }
+        if (n != 0 && cnt == 0)
+        {
+            printf("Goldbach's conjecture is wrong.\n");
+            continue;
+        }
+    }
+    return 0;
+}
