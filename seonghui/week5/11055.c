6681d20b963aa19ed3ad9fe59fe8753dffadefe9
seonghui <msh9980@naver.com>
2022-11-15 19:55:50 +0900

    [BOJ] / 가장 큰 증가 부분 수열 / 15m

---
+++ seonghui/week5/11055.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int dp[1001];
+int a[1001];
+
+int increase(int n)
+{
+    int max;
+    max = a[0];
+    for (int i = 0; i < n; i++) // /1~n까지 수열 전체 도는 반복문
+    {
+        dp[i] = a[i];
+        for (int j = 0; j < i; j++) // 0 ~ i-1까지 돌면서 i앞에 있는 모든 수열을 도는 반복문
+        {
+            if (a[i] > a[j] && dp[i] < dp[j] + a[i]) // 현재 수열에서 i인덱스에 위치하는 값보다 더 작은 값이 면 증가
+            // 그리고 dp의 i번째 인덱스보다 j의 인덱스가 더 클때만 갱신
+            {
+                dp[i] = dp[j] + a[i];
+                if (dp[i] > max) //가장 길이 큰 수열 찾기
+                    max = dp[i];
+            }
+        }
+        // printf("[%d %d]\n", dp[i], max);
+    }
+    printf("%d", max);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    increase(n);
+}
